d6264f377b6708127e5713674aa9c2b8cee5ce83
Bola_sw25@student.itstep.kz <Bola_sw25@student.itstep.kz>
2021-03-01 11:11:46 +0600

    Добавьте файлы проекта.

---
+++ InformationXmlHW/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Xml;
+
+namespace InformationXmlHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load("С://users.xml");
+            XmlElement xRoot = xDoc.DocumentElement;
+            foreach (XmlNode xnode in xRoot)
+            {
+                if (xnode.Attributes.Count > 0)
+                {
+                    XmlNode attr = xnode.Attributes.GetNamedItem("name");
+                    if (attr != null)
+                        Console.WriteLine(attr.Value);
+                }
+                foreach (XmlNode childnode in xnode.ChildNodes)
+                {
+                    if (childnode.Name == "company")
+                    {
+                        Console.WriteLine($"Компания: {childnode.InnerText}");
+                    }
+                    if (childnode.Name == "age")
+                    {
+                        Console.WriteLine($"Возраст: {childnode.InnerText}");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ InformationXmlHW/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InformationXmlHW
+{
+    public class Student
+    {
+        private String course, specialization;
+
+        public Student(String name, String course, String specialization)
+        {
+            super(name);
+            this.course = course;
+            this.specialization = specialization;
+        }
+
+        private void super(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public String getCourse()
+        {
+            return course;
+        }
+
+        public String getSpecialization()
+        {
+            return specialization;
+        }
+
+        public String toString()
+        {
+            return "Студент отличник " + getName() + " " + course + "-го курса, обучающийся по специальности " + specialization;
+        }
+
+        private string getName()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
